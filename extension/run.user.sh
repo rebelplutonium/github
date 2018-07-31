73e09210d9d35c5bb8d8e73019537f38d3e5ca14
Emory Merryman <emory.merryman@gmail.com>
2018-07-31 17:33:22 +0000

    fix error with WORKSPACE

--- extension/run.user.sh
+++ extension/run.user.sh
@@ -10,5 +10,4 @@ EOF
     chmod 0700 /home/user/.ssh &&
     touch /home/user/.ssh/config /home/user/.ssh/known_hosts /home/user/.ssh/upstream_id_rsa /home/user/.ssh/origin_id_rsa /home/user/.ssh/report_id_rsa &&
     chmod 0600 /home/user/.ssh/upstream_id_rsa /home/user/.ssh/origin_id_rsa /home/user/.ssh/report_id_rsa /home/user/.ssh/config &&
-    chmod 0644 /home/user/.ssh/known_hosts &&
-    git -C /opt/docker/workspace init
+    chmod 0644 /home/user/.ssh/known_hosts
